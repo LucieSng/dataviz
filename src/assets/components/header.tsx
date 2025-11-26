a23f5a641c9b29c14fbfbdd4a579d430ef60242a
piouoiq <melargilliere@gmail.com>
2025-11-26 15:48:07 +0100

    add header

---
+++ src/assets/components/header.tsx
@@ -0,0 +1,9 @@
+const HeaderComponent = () => (
+  <header className="flex bg-amber-800">
+    <button type="button">accueuil</button>
+    <h1>Dataviz, vos données ciné à Paris</h1>
+    <button>Analyses graphiques</button>
+  </header>
+);
+
+export default HeaderComponent;
