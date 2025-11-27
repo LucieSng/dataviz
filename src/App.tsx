0e8042e27aaf8616dcfef94ece9617f9d611d533
Lucie Seng <lucie.sng@gmail.com>
2025-11-27 11:54:24 +0100

    feat : base lineChartYear

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 // On importe le hook qui va récupérer les données de l'API
-import useParisApi from "./assets/components/hooks/useParisApi";
+// import useParisApi from "./assets/components/hooks/useParisApi";
 import LineChartYear from "./assets/components/LineChartYear";
 
 import "./App.css";
 
 function App() {
   //le hook "useParisApi" retourne un objet contenant la donnée récupérée avec l'appel d'api et la propriété {apidata} permet d'extraire celle ci de cet objet.
-  const { apiData } = useParisApi();
+  // const { apiData } = useParisApi();
   return (
     <>
       <LineChartYear></LineChartYear>
--- src/assets/components/LineChartYear.tsx
+++ src/assets/components/LineChartYear.tsx
@@ -11,87 +11,67 @@ import {
   Line,
 } from "recharts";
 
-// #récupérer la data de l'api et l'afficher sur App.tsx
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+import useParisApi from "./hooks/useParisApi";
 
-const LineChartYear = ({ isAnimationActive = false }) => (
-  <LineChart
-    style={{
-      width: "100%",
-      maxWidth: "700px",
-      maxHeight: "70vh",
-      aspectRatio: 1.618,
-    }}
-    responsive
-    data={data}
-    margin={{
-      top: 5,
-      right: 30,
-      left: 20,
-      bottom: 5,
-    }}
-  >
-    <CartesianGrid strokeDasharray="3 3" />
-    <XAxis dataKey="name" />
-    <YAxis width="auto" />
-    <Tooltip />
-    <Legend />
-    <Line
-      type="monotone"
-      dataKey="pv"
-      stroke="#8884d8"
-      isAnimationActive={isAnimationActive}
-    />
-    <Line
-      type="monotone"
-      dataKey="uv"
-      stroke="#82ca9d"
-      isAnimationActive={isAnimationActive}
-    />
-  </LineChart>
-);
+// Récupérer la data de l'api et l'afficher sur App.tsx
+// Grouper par année, récupérer que les années
+// Compter le nombre de tournages par année avec "count"
+// Trier par année croissante avec "sort"
+
+const LineChartYear = ({ isAnimationActive = false }) => {
+  // On définit une URL spécifique pour ce graphique qui récupère les tournages groupés par année
+  const apiUrl =
+    "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/lieux-de-tournage-a-paris/records?select=annee_tournage,count(*)&group_by=annee_tournage&limit=100";
+  const { apiData } = useParisApi(apiUrl);
+
+  // Message pour le chargement des données
+  if (!apiData?.results) {
+    return <div>Chargement des données...</div>;
+  }
+
+  // Transformation des données
+  const chartData = apiData.results.map((item: any) => ({
+    name: item.annee_tournage,
+    tournages: item.count,
+  }));
+
+  // Afficher le graph
+  return (
+    <LineChart
+      style={{
+        width: "100%",
+        maxWidth: "700px",
+        maxHeight: "70vh",
+        aspectRatio: 1.618,
+      }}
+      responsive
+      data={apiData}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis width="auto" />
+      <Tooltip />
+      <Legend />
+      <Line
+        type="monotone"
+        dataKey="pv"
+        stroke="#8884d8"
+        isAnimationActive={isAnimationActive}
+      />
+      <Line
+        type="monotone"
+        dataKey="uv"
+        stroke="#82ca9d"
+        isAnimationActive={isAnimationActive}
+      />
+    </LineChart>
+  );
+};
 
 export default LineChartYear;
