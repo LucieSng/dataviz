acfa1c2fb89e72c6dade6792416facbaa258d7f4
Lucie Seng <lucie.sng@gmail.com>
2025-12-04 22:24:52 +0100

    feat : link analyse page

--- src/assets/components/header.tsx
+++ src/assets/components/header.tsx
@@ -1,9 +1,11 @@
 const HeaderComponent = () => (
   <header className="navbar bg-blue-700">
     <div className="flex-1">
-      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl btn-ghost">
-        Accueil
-      </button>
+      <Link to="/analysesPage">
+        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl btn-ghost">
+          Accueil
+        </button>
+      </Link>
     </div>
     <div className="flex-1">
       <h1>Dataviz, vos données ciné à Paris</h1>
