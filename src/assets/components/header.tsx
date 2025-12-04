4e0054ffd14fa7220326f9a1dafd14cc73759ad2
Lucie Seng <lucie.sng@gmail.com>
2025-12-04 21:26:47 +0100

    fix : clean structure

--- src/assets/components/header.tsx
+++ src/assets/components/header.tsx
@@ -1,5 +1,5 @@
 const HeaderComponent = () => (
-  <header className="navbar bg-violet-900">
+  <header className="navbar bg-blue-700">
     <div className="flex-1">
       <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl btn-ghost">
         Accueil
@@ -9,11 +9,7 @@ const HeaderComponent = () => (
       <h1>Dataviz, vos données ciné à Paris</h1>
     </div>
 
-    <div className="flex-none">
-      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl btn-ghost">
-        Analyses graphiques
-      </button>
-    </div>
+    <div className="flex-none"></div>
   </header>
 );
 export default HeaderComponent;
