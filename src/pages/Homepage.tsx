e226e2ddbf693f1b8cc41eb51f62740cac09db24
Lucie Seng <lucie.sng@gmail.com>
2025-12-04 16:38:26 +0100

    fix : clean file

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,1 +1,14 @@
+import HeaderComponent from "./assets/components/header";
+import FooterComponent from "./assets/components/footer";
+import "./App.css";
+
+function Homepage() {
+  return (
+    <>
+      <HeaderComponent></HeaderComponent>
+      <FooterComponent></FooterComponent>
+    </>
+  );
+}
+
 export default Homepage;
