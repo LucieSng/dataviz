c463c942c38b3b1d5d2dfb3747794005ccc839ac
Lucie Seng <lucie.sng@gmail.com>
2025-12-05 12:12:58 +0100

    fix : delete back to homepage

--- src/assets/components/header.tsx
+++ src/assets/components/header.tsx
@@ -1,25 +1,12 @@
-import { useNavigate } from "react-router-dom";
+const HeaderComponent = () => (
+  <header className="navbar bg-blue-700">
+    <div className="flex-1"></div>
+    <div className="flex-1">
+      <h1>Dataviz, vos données ciné à Paris</h1>
+    </div>
 
-export default function HeaderComponent() {
-  const nav = useNavigate();
-  const navigate = () => {
-    nav("/Homepage");
-  };
-  return (
-    <header className="navbar bg-blue-700">
-      <div className="flex-1">
-        <button
-          onClick={navigate}
-          className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl btn-ghost"
-        >
-          Accueil
-        </button>
-      </div>
-      <div className="flex-1">
-        <h1>Dataviz, vos données ciné à Paris</h1>
-      </div>
+    <div className="flex-none"></div>
+  </header>
+);
 
-      <div className="flex-none"></div>
-    </header>
-  );
-}
+export default HeaderComponent;
