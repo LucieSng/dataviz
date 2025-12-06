ed7d1b3febab513ab8ecb64448eb52c0d61a1743
Lucie Seng <lucie.sng@gmail.com>
2025-12-06 20:40:16 +0100

    style : update style

--- src/assets/components/footer.tsx
+++ src/assets/components/footer.tsx
@@ -1,40 +1,44 @@
 const FooterComponent = () => (
-  <footer className="bg-violet-900 text-white px-6 py-10 flex flex-col gap-10 lg:flex-row lg:justify-between">
-    <div className="container-description">
-      <div className="">
-        Ce projet a pour objectif de rendre visible l’activité cinématographique
-        française à partir des données ouvertes de la Ville de Paris.
-        <a
-          href="https://github.com/LucieSng/dataviz"
-          className="font-medium text-fg-brand hover:underline"
-        >
-          Lien du repository
-        </a>
+  <footer className="bg-blue-700 text-white py-12">
+    <div className="container mx-auto">
+      <div className="grid grid-cols-1 grid-cols-2 gap-32">
+        <div>
+          <h1 className="font-regular text-xl mb-4">
+            Ce projet a pour objectif de rendre visible l’activité
+            cinématographique française à partir des données ouvertes de la
+            Ville de Paris.
+          </h1>
+          <a
+            href="https://github.com/LucieSng/dataviz"
+            className="font-bold underline"
+          >
+            Découvrir le repository
+          </a>
+        </div>
+        <div>
+          <h1 className="font-medium mb-3">
+            Mélissa Largilliere •
+            <a
+              href="https://github.com/piouoiq"
+              className="font-bold ml-2 underline"
+            >
+              Git
+            </a>
+          </h1>
+          <h1 className="font-medium mb-3">
+            Lucie Seng •
+            <a
+              href="https://github.com/LucieSng"
+              className="font-bold ml-2 underline"
+            >
+              Git
+            </a>
+          </h1>
+          <p className="font-medium">
+            Promotion Grace Hopper • Ada Tech School
+          </p>
+        </div>
       </div>
-    </div>
-    <div className="container-author">
-      <h1 className="">Mélissa Largilliere</h1>
-      <p className="text-body font-medium">
-        <a
-          href="https://github.com/piouoiq"
-          className="font-medium text-fg-brand hover:underline"
-        >
-          Lien Git
-        </a>
-      </p>
-      <h1>Lucie Seng</h1>{" "}
-      <p className="text-body font-medium ">
-        <a
-          href="https://github.com/LucieSng"
-          className="font-medium text-fg-brand hover:underline"
-        >
-          Lien Git
-        </a>
-      </p>
-    </div>
-
-    <div className="container-">
-      <p className="font-medium">Promotion Grace Hopper - Ada Tech School</p>
     </div>
   </footer>
 );
